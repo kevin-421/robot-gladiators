bd5aeae404e7ec5498e45af331eb1ee98715db62
kevin pena <kevinpena421@gmail.com>
2022-03-08 15:23:21 -0600

    while loop issue

--- assets/assets/game.js
+++ assets/assets/game.js
@@ -9,33 +9,31 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-function fight (enemyName) {
+var fight = function(enemyNames) {
 
- // while(enemyHealth > 0) {
-  //  Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  while(enemyHealth > 0) {
 
-  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
 
   // if player choses to fight, then fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -63,6 +61,9 @@ function fight (enemyName) {
   else {
     fight(); }
   }
+}
 
-
-for (var i = 0; i < enemyNames.length; i++) {};
+for (var i = 0; i < enemyNames.length; i++) {
+  debugger;
+  fight(enemyNames[1]);
+};
